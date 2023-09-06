8edc22a7b0028ea336c590df6e5d90b66fdae97f
Dagim Damtew <78877931+DagMT@users.noreply.github.com>
2023-09-06 13:07:29 +0300

    feat: Country Model Setup

--- models/countryModel.js
+++ models/countryModel.js
@@ -1,6 +1,138 @@
 const mongoose = require('mongoose');
 
+//Common Schemas
+const requirementSchema = new mongoose.Schema({
+  basicRequirements: {
+    type: Object,
+    name: String,
+    Description: String,
+  },
+  undergradRequirements: {
+    type: Object,
+    name: String,
+    Description: String,
+  },
+  gradRequirements: {
+    type: Object,
+    name: String,
+    Description: String,
+  },
+  highSchoolRequirements: {
+    type: Object,
+    name: String,
+    Description: String,
+  },
+});
+
+const informationSchema = new mongoose.Schema({
+  description: String,
+  startDate: Date,
+  endDate: Date,
+  season: String,
+  year: Number,
+  availability: Boolean,
+  level: String,
+  applicationExpiryDate: Date,
+  maxApplicantsNumber: Number,
+  currentApplicantsNumber: Number,
+  duration: {
+    type: Object,
+    label: String,
+    length: Number,
+  },
+  requirements: { type: requirementSchema },
+});
+
+//Visit Packages
+const visitPackageSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A package must have a name'],
+    unique: true,
+    trim: true,
+  },
+  information: {
+    type: informationSchema,
+  },
+  duration: {
+    type: String,
+    label: String,
+    length: String,
+  },
+  applicants: [String],
+});
+
+//Educational Packages
+const availableIntakeSchema = new mongoose.Schema({
+  intakeSeason: { type: [String] },
+  intakeYear: Number,
+});
+
+const programSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A country must have a name'],
+    unique: true,
+    trim: true,
+  },
+  description: String,
+  specialRequirement: {
+    type: Object,
+    name: String,
+    description: String,
+  },
+  availability: Boolean,
+  availableIntakes: { type: [availableIntakeSchema] },
+});
+
+const educationCostSchema = new mongoose.Schema({
+  description: String,
+  minimumTuitionCost: Number,
+  MaximumTuitionCost: Number,
+  averageLivingCost: Number,
+  scholarships: [
+    {
+      type: Object,
+      name: String,
+      minimumAmount: Number,
+      maximumAmount: Number,
+    },
+  ],
+});
+
+const educationalPackageSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A package must have a name'],
+    unique: true,
+    trim: true,
+  },
+  information: { type: informationSchema },
+  programs: {
+    type: [programSchema],
+  },
+  scholarship: {
+    type: educationCostSchema,
+  },
+  applicants: [String],
+});
+
+//Consultation Packages
+const consultationPackageSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A consultation package must have a name'],
+    unique: true,
+    trim: true,
+  },
+  services: [{ type: Object, name: String, description: String }],
+  priceStartingFrom: Number,
+});
+
+//MAIN SCHEMA ---- MODEL
+//Country Schema
 const countrySchema = new mongoose.Schema({
+  _id: new mongoose.Types.ObjectId(),
   name: {
     type: String,
     required: [true, 'A country must have a name'],
@@ -9,11 +141,10 @@ const countrySchema = new mongoose.Schema({
   },
   slug: {
     type: String,
-    required: false,
     unique: true,
     trim: true,
   },
-  imageCover: {
+  flagImage: {
     type: String,
     required: [true, 'A country must have a flag image'],
   },
@@ -26,7 +157,21 @@ const countrySchema = new mongoose.Schema({
     type: [String],
   },
   createdAt: { type: Date, default: Date.now() },
-  startDates: [Date],
+  visitPackages: {
+    type: [visitPackageSchema],
+  },
+  educationalPackages: {
+    type: [educationalPackageSchema],
+  },
+  consultationPackages: {
+    type: [consultationPackageSchema],
+  },
+  aboutCountry: {
+    type: Object,
+    currency: String,
+    currencySymbol: String,
+    description: String,
+  },
 });
 
 const Country = mongoose.model('Country', countrySchema);
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const dotenv = require("dotenv");
-const express = require("express");
-dotenv.config({ path: "./config.env" });
-const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const express = require('express');
+dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
 
 const app = express();
 
 const DB_URL = process.env.DATABASE.replace(
-  "<PASSWORD>",
+  '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
 mongoose
   .connect(DB_URL)
-  .then((con) => console.log("DB connection established successfully"));
+  .then(() => console.log('DB connection established successfully'));
 
 //SERVER
 const port = process.env.PORT || 5000;
-app.listen(port, "localhost", () => {
+app.listen(port, 'localhost', () => {
   console.log(`Server listening on ${port}`);
 });
